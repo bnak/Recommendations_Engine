c2d792e3f807fac86010db91c38286cc964317a9
bnak <bethnakamura@Student-MacBook-Pro.local>
2014-07-14 16:18:29 -0700

    Working dictionaries branch

--- build_matrix.py
+++ build_matrix.py
@@ -12,15 +12,15 @@ Ratings: Movies x Users
 
 """
 
-def build_matrix(rows, columns, space_filler):
-	#rows are items, columns are users
-	#ratings[item_id-1][user_id-1]
-	matrix = [space_filler]*rows
-	for i in xrange(rows):
-		matrix[i] = [space_filler]*columns
+# def build_matrix(rows, columns, space_filler):
+# 	#rows are items, columns are users
+# 	#ratings[item_id-1][user_id-1]
+# 	matrix = [space_filler]*rows
+# 	for i in xrange(rows):
+# 		matrix[i] = [space_filler]*columns
 
-	matrix = np.array(matrix)
-	return matrix
+# 	matrix = np.array(matrix)
+# 	return matrix
 
 def read_movie_rating(my_file):
 	f = open(my_file)
@@ -29,67 +29,67 @@ def read_movie_rating(my_file):
 	return lines
 
 
-def fill_matrix(lines, ratings):
+# def fill_matrix(lines, ratings):
 
-	#get movie_id from filetext of lines; corresponds to first index
-	#of ratings matrix
-	line1 = lines[0].split(":", 1)
-	movie_id=int(line1[0])
+# 	#get movie_id from filetext of lines; corresponds to first index
+# 	#of ratings matrix
+# 	line1 = lines[0].split(":", 1)
+# 	movie_id=int(line1[0])
 
-	lines.pop(0)  #remove first line with movie_id
-
-
-	for i in xrange(len(lines)):
-
-		individual_rating = lines[i].split(",")
-		#individual_rating = [user_id, rating, date]
-
-		user_id = int(individual_rating[0])
-		rating = int(individual_rating[1])
+# 	lines.pop(0)  #remove first line with movie_id
 
 
-		ratings[movie_id-1][user_id-1]=rating
+# 	for i in xrange(len(lines)):
 
-	return ratings
+# 		individual_rating = lines[i].split(",")
+# 		#individual_rating = [user_id, rating, date]
 
-def build_ratings_matrix_from_file(directory_name, num_users, space_filler):
-
-	files = os.listdir(directory_name)
-	files.pop(0)  #first entry is .ds_store
-
-	#users have to be hardcoded in
-
-	ratings = build_matrix(len(files), num_users, space_filler)
-
-	for filename in files:
-		lines = read_movie_rating(directory_name + "/" + filename)
-		fill_matrix(lines, ratings)
-
-	return ratings
+# 		user_id = int(individual_rating[0])
+# 		rating = int(individual_rating[1])
 
 
-def fill_matrix_with_average_ratings(ratings):
-	for i in xrange(len(ratings)):
-		average_rating = np.mean(ratings[i])
-		for j in xrange(len(ratings[i])):
-			if ratings[i][j] == 0:
-				ratings[i][j] = average_rating
+# 		ratings[movie_id-1][user_id-1]=rating
 
-	return ratings
+# 	return ratings
 
-def retrieve_folder_data(directory_name, movie_id, line_num):
-	files = os.listdir(directory_name)
-	files.pop(0)  #first entry is ds.store
+# def build_ratings_matrix_from_file(directory_name, num_users, space_filler):
 
-	filepath = directory_name + "/" + files[movie_id-1]
+# 	files = os.listdir(directory_name)
+# 	files.pop(0)  #first entry is .ds_store
 
-	individual_rating = linecache.getline(filepath, line_num).split(",")
+# 	#users have to be hardcoded in
 
-	user_id = int(individual_rating[0])
+# 	ratings = build_matrix(len(files), num_users, space_filler)
 
-	rating = int(individual_rating[1])
+# 	for filename in files:
+# 		lines = read_movie_rating(directory_name + "/" + filename)
+# 		fill_matrix(lines, ratings)
 
-	return user_id, rating
+# 	return ratings
+
+
+# def fill_matrix_with_average_ratings(ratings):
+# 	for i in xrange(len(ratings)):
+# 		average_rating = np.mean(ratings[i])
+# 		for j in xrange(len(ratings[i])):
+# 			if ratings[i][j] == 0:
+# 				ratings[i][j] = average_rating
+
+# 	return ratings
+
+# def retrieve_folder_data(directory_name, movie_id, line_num):
+# 	files = os.listdir(directory_name)
+# 	files.pop(0)  #first entry is ds.store
+
+# 	filepath = directory_name + "/" + files[movie_id-1]
+
+# 	individual_rating = linecache.getline(filepath, line_num).split(",")
+
+# 	user_id = int(individual_rating[0])
+
+# 	rating = int(individual_rating[1])
+
+# 	return user_id, rating
 
 def file_len(my_file):
 	with open(my_file) as f:
@@ -98,7 +98,7 @@ def file_len(my_file):
 	return i+1
 
 
-def matrix_factorization_from_file(directory_name, K, iterations, alpha, beta, num_users):
+def matrix_factorization_from_file(directory_name, K, iterations, alpha, beta, num_users, ratings_dictionary):
 	"""
 
 	INPUT:
@@ -113,7 +113,6 @@ def matrix_factorization_from_file(directory_name, K, iterations, alpha, beta, n
 	    Q     : an initial matrix of dimension N x K (users x features)
 
 	"""
-	A = []
 
 	files = os.listdir(directory_name)
 	files.pop(0)  #removes first entry of ds.store
@@ -128,55 +127,101 @@ def matrix_factorization_from_file(directory_name, K, iterations, alpha, beta, n
 	for step in xrange(iterations):
 		for i in xrange(num_movies): 
 
-			user_list = [0] * num_users
 
-			A.append(user_list)
+			for j in xrange(num_users):
 
-			filepath = directory_name + "/" + files[i]
-
-
-			for j in xrange(2,file_len(filepath)):
-				individual_rating = linecache.getline(filepath, j).split(",")
-
-				#user_id = int(individual_rating[0])
-			
-
-				rating = int(individual_rating[1])
-
-				j = j-1
-
-				A[i][j] = rating
+				rating = ratings_dictionary.get((i, j), 0)
 
 
-				if A[i][j] > 0:
-					eij = A[i][j] - np.dot(P[i,:], Q[:,j])
+				if rating > 0:
+					eij = rating - np.dot(P[i,:], Q[:,j])
 					for k in xrange(K):
 						P[i][k] = P[i][k] + alpha * (2 * eij *Q[k][j] - beta * P[i][k])
-						# P.set(i, k, (P.get(i, k) + alpha * (2 * eij *Q[k][j] - beta * P[i][k])
 						
 						Q[k][j] = Q[k][j] + alpha * (2 * eij * P[i][k] - beta * Q[k][j])
 
 		error_prime = 0 
 
-		for i in xrange(len(A)):
-			for j in xrange(len(A[i])):
-				if A[i][j]>0:
+		for i in xrange(num_movies):
+			for j in xrange(num_users):
+				if rating>0:
 					error_prime = error_prime + pow(A[i][j] - np.dot(P[i,:], Q[:,j]),2)
 					for k in xrange(K):
 						error_prime = error_prime + (beta/2) * (pow(P[i][k],2)+pow(Q[k][j],2))
 			if error_prime < 0.001:
 				break
 
-
-	print "Size of A: %s" % sys.getsizeof(A)
-
 	return P, Q.T
 
-def create_dictionary_of_ratings(movie_id):
+def create_dictionary_of_ratings(directory_name, movie_id, movie_ratings_dictionary):
+
+	 
+
+	files = os.listdir(directory_name)
+
+	files.pop(0)  #first entry is ds.store
+
+	filepath = directory_name + "/" + files[movie_id-1]
+
+	#individual_rating = linecache.getline(filepath, line_num).split(",")
+
+	ratings = read_movie_rating(filepath)
+
+	for i in xrange(2,file_len(filepath)):
+		individual_rating = linecache.getline(filepath, i).split(",")
+		user_id = int(individual_rating[0])	
+
+		rating = int(individual_rating[1])
+
+		key = (movie_id, user_id)
+		movie_ratings_dictionary[key] = rating
 
 
 
-def prediction(movie_id, user_id): 
+	return movie_ratings_dictionary
+
+
+
+def calculate_prediction(directory_name, movie_id, user_id, num_movies, num_users): 
+
+	ratings = {}
+
+	ratings = create_dictionary_of_ratings(directory_name, movie_id, ratings)
+
+
+
+	P, Q = matrix_factorization_from_file(directory_name=directory_name, 
+		K=2, iterations=5000, alpha=.0002, beta=.0002, 
+		num_users=30, ratings_dictionary=ratings)
+
+	
+	vector_2 = [Q[user_id]]
+
+
+	predicted_rating = np.dot(P[movie_id], vector_2[0].T)
+
+
+
+	return predicted_rating
+
+
+def retrieve_rating(directory_name, movie_id, user_id, num_movies, num_users):
+
+	ratings = {}
+
+	ratings_dictionary = create_dictionary_of_ratings(directory_name, movie_id, ratings)
+
+	individual_rating = ratings_dictionary.get((movie_id, user_id), 0)
+
+	if individual_rating>0: 
+
+		print "Previously rated: %s" % individual_rating
+		return individual_rating
+
+	else: 
+		individual_rating = calculate_prediction(directory_name, movie_id, user_id, num_movies, num_users)
+
+		print "Predicted rating: %s" % individual_rating
 
 
 
@@ -187,46 +232,19 @@ def main():
 
 	start_time=datetime.datetime.now()
 
-<<<<<<< HEAD
-	# ratings = build_ratings_matrix_from_file("netflix_initial_test_data", 10, 0)
+	movie_ratings_dictionary = {}  
 
-	
-	# print "Ratings: "
-	# print ratings
-
-	# P, Q = matrix_factorization(ratings, K = 2, iterations=5000, alpha=0.0002, beta=0.02)
-=======
-	ratings = build_ratings_matrix_from_file("netflix_local_data", 30, 0)
-
-	print ratings
+	#(movie i, user j) = rating
+	movie_id = 3
+	user_id = 29
 
 
-	print "Size of Ratings: %s" % sys.getsizeof(ratings)
 
-	P, Q = matrix_factorization_from_file("netflix_local_data", K = 2, iterations=5000, alpha=0.0002, beta=0.02, num_users = 30)
->>>>>>> single_file_access
+	retrieve_rating("netflix_local_data", movie_id, user_id, 20, 30)
 
-	# print "************************************************"
-	# print "Estimated_ratings:"
 
-	# predicted_ratings = np.dot(P, Q.T)
 
-<<<<<<< HEAD
-	# print predicted_ratings
-=======
->>>>>>> single_file_access
 
-	# average_error = np.mean(predicted_ratings-fill_matrix_with_average_ratings(ratings))
-
-<<<<<<< HEAD
-	# print "Average Error: %s" % average_error
-	
-
-=======
-	print "Size of prediected ratings: %s"  % sys.getsizeof(predicted_ratings)
->>>>>>> single_file_access
-
-	print "Average Error: %s" % average_error
 
 	end_time = datetime.datetime.now()
 
